c3c3c7792f8f682b86a12591d9f3ab17aa49c2bb
NirmalCandy <nirmal@complyance.io>
2023-06-15 19:40:54 +0530

    config commit
    config commit

---
+++ dynamodb/src/main/java/com/nirmaldynamodbapi/config/DynamoDBConfig.java
@@ -0,0 +1,32 @@
+package com.nirmaldynamodbapi.dynamodb.config;
+
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DynamoDBConfig {
+
+    public static final String SERVICE_ENDPOINT = "https://dynamodb.ap-south-1.amazonaws.com";
+    public static final String REGION = "ap-south-1";
+    public static final String ACCESS_KEY = "AKIA3UUB6FCQWTMCHZBB";
+    public static final String SECRET_KEY = "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";
+
+    @Bean
+    public DynamoDBMapper mapper(){
+        return new DynamoDBMapper(amazonDynamoDb());
+    }
+
+    private AmazonDynamoDB amazonDynamoDb() {
+        return AmazonDynamoDBClientBuilder.standard()
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(SERVICE_ENDPOINT, REGION))
+                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(ACCESS_KEY, SECRET_KEY))).build();
+
+    }
+}
